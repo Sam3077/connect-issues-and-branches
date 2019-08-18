06a181d1d46bc78d04da566cb619e89ebf9d1303
Sam Hirsch <sam.hirsch66@gmail.com>
2019-08-17 21:34:42 -0600

    fixed regex match

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,30 @@ import { isNull } from "util";
 
 export = (app: Application) => {
   app.on("create", async context => {
-    const payload = context.payload;
     const config = await context.config("config.yml");
+    const branchNameRegex = new RegExp(
+      config.branchNameRegex || "^[A-Za-z/-_]*[0-9]+[A-Za-z0-9/-_]*$",
+      "g"
+    );
+    const issueReferenceRegex = new RegExp(
+      config.issueReferenceRegex || "[0-9]+"
+    );
 
+    const payload = context.payload;
     if (!payload.ref_type || payload.ref_type !== "branch") return;
 
     const branchName: string = payload.ref;
-    if (!branchName.match(config.branchNameRegex)) return;
+    context.log(config.branchNameRegex);
+    if (branchName.match(branchNameRegex) === null) {
+      context.log("doesn't match branch regex");
+      return;
+    }
 
-    const regexMatch = branchName.match(config.issueReferenceRegex);
-    if (isNull(regexMatch) || regexMatch.length == 0) return;
+    const regexMatch = branchName.match(issueReferenceRegex);
+    if (isNull(regexMatch) || regexMatch.length == 0) {
+      context.log("no issue number found");
+      return;
+    }
 
     const issueReference = Number.parseInt(regexMatch[0]);
 
